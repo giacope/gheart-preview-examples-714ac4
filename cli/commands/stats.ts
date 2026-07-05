73eb783f694105e77ce65f00f971f75a6f6f3102
Giacomo Guiulfo <giaco@hey.com>
2026-07-05 16:21:24 +0900

    feat: add an acme stats subcommand

---
+++ cli/commands/stats.ts
@@ -0,0 +1,3 @@
+export function stats(): void {
+  console.log('orders: 128\nusers:  42\nuptime: 99.9%');
+}
--- cli/index.ts
+++ cli/index.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { hello } from './commands/hello';
+import { stats } from './commands/stats';
 
 const [cmd, ...args] = process.argv.slice(2);
 switch (cmd) {
   case 'hello':
     hello(args);
     break;
+  case 'stats':
+    stats();
+    break;
   default:
-    console.log('usage: acme <hello> [options]');
+    console.log('usage: acme <hello|stats> [options]');
 }
